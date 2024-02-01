06cc31f5d0a524731db0f54426dbd71b2449b7cb
pappalaity <pappalaity@gmail.com>
2024-02-01 16:49:25 +0000

    Correctifs Services Displaying

--- src/components/about/AboutServiceSingle.vue
+++ src/components/about/AboutServiceSingle.vue
@@ -5,15 +5,15 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="w-42 sm:w-64 p-2 bg-ternary-light dark:bg-ternary-dark rounded-lg">
     <!--    <a :href="client.url">-->
     <img
         :src="client.img"
         :alt="client.title"
-        class="w-42 sm:w-64 py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-4 cursor-pointer dark:bg-secondary-light"
+        class="py-4 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-4 cursor-pointer dark:bg-secondary-light"
     />
-    <div class="w-42 sm:w-64">
-      <span class="text-ternary-dark dark:text-ternary-light pr-16">
+    <div class="text-center">
+      <span class="text-ternary-dark dark:text-ternary-light text-center">
       {{ client.description }}
     </span>
     </div>
