fa7815a9e6998d876ef3057ec2c7112041162e2d
pappalaity <pappalaity@gmail.com>
2024-02-01 16:39:58 +0000

    Carrieres Page Done

--- src/views/Carrieres.vue
+++ src/views/Carrieres.vue
@@ -12,12 +12,10 @@ export default {
 </script>
 
 <template>
-  <!--  <div class="md:flex md:items-center md:justify-between">-->
   <div>
     <div class="container mx-auto">
       <Jobs/>
     </div>
-    <!--    <div class="sm:max-w-sm md:max-w-xs container mx-auto">-->
     <div class="container mx-auto">
       <Etudes/>
     </div>
